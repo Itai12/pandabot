f4b912ca0252a1dc858f669ead32392c1fea3e4c
itai <salutevg@gmail.com>
2020-12-13 17:47:23 +0100

    commit

--- bot3.py
+++ bot3.py
@@ -11,11 +11,21 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 @client.event
 async def on_message(message):
-	if "<@!787710618038698034>" in message.content:
-		http = urllib3.PoolManager()
-		url = "https://some-random-api.ml/img/red_panda"
-		r = http.request('GET', url)
-		data=json.loads(r.data.decode('utf-8'))
-		await message.channel.send(str(data)[10:])
+	if message.content.startswith("<@!787710618038698034>"):
+		inte=message.content[23:]
+		try:
+			x=int(inte)
+			for i in range(x):
+				http = urllib3.PoolManager()
+				url = "https://some-random-api.ml/img/red_panda"
+				r = http.request('GET', url)
+				data=json.loads(r.data.decode('utf-8'))
+				await message.channel.send(str(data)[10:])
+		except Exception as e:
+			http = urllib3.PoolManager()
+			url = "https://some-random-api.ml/img/red_panda"
+			r = http.request('GET', url)
+			data=json.loads(r.data.decode('utf-8'))
+			await message.channel.send(str(data)[10:])
 	await client.process_commands(message)
 client.run('Nzg3NzEwNjE4MDM4Njk4MDM0.X9Y6rg.0IN3V7vwKK_s3ssZo9unfJUsxS4')
